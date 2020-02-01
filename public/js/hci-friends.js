f3c7ee892688078d856f6e044ee13ffb3ac48d50
Luis Escobar <ljmatias@ucsd.edu>
2020-01-31 17:19:05 -0800

    Finished Lab

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+	$('.friend-name').click(changeText);
 }
 
 function anagrammedName(name) {
@@ -43,4 +44,9 @@ function anagrammedName(name) {
 		console.log(name + " not known for anagramming.");
 		return name;
 	}
+}
+
+function changeText(event){
+	event.preventDefault();
+	$(this).text(anagrammedName($(this).text()));
 }
